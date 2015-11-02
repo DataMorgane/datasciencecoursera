3e0781f43222bd4e2a31bf1fcd5df9e4bb7806f2
Morgane <morgane.flauder@gmail.com>
2015-11-02 22:51:23 +0100

    Deleting useless step

--- Exploratory_Data_Analysis/Project_1/plot1.R
+++ Exploratory_Data_Analysis/Project_1/plot1.R
@@ -1,5 +1,5 @@
 
-# Code constructiong plot1.png
+# Code constructing plot1.png
 
 data <- read.table("household_power_consumption.txt", 
                    skip = 66645, nrows = 2879, 
@@ -10,10 +10,6 @@ data <- read.table("household_power_consumption.txt",
                                "Global_reactive_power","Voltage",
                                "Global_intensity","Sub_metering_1",
                                "Sub_metering_2","Sub_metering_3"))
-
-
-data$Time <- strptime(paste(data$Date, data$Time), 
-                     format = "%d/%m/%Y %H:%M:%S")
 
 png("plot1.png", width = 480, height = 480)
 
